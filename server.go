7af3b0b40b6890a9805cc9b0cbb578049a447f98
anajmeld <najmeldeen.adamabkar@ynov.com>
2023-10-30 10:55:41 +0100

    server

--- server.go
+++ server.go
@@ -1,1 +1,20 @@
-ff 
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	serverAddr := "127.0.0.1:8080"
+	conn, err := net.Dial("tcp", serverAddr)
+	if err != nil {
+		fmt.Println("Erreur lors de la connexion au serveur:", err)
+		return
+	}
+	defer conn.Close()
+
+	serverIP, _, _ := net.SplitHostPort(conn.RemoteAddr().String())
+	fmt.Println("Adresse IP du serveur:", serverIP)
+	fmt.Println("Port du serveur:", serverAddr)
+}
