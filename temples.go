7e1b68bf6ac391b2230aa4c6efce07794464a23c
anajmeld <najmeldeen.adamabkar@ynov.com>
2023-11-06 08:47:22 +0100

    server

--- temples.go
+++ temples.go
@@ -24,24 +24,6 @@ func getRandomWord() string {
 func HangmanHandler(w http.ResponseWriter, r *http.Request) {
 	// Générez un mot aléatoire.
 	wordToGuess := getRandomWord()
-
-	// Créez un modèle HTML simple.
-	tmpl := `
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Jeu du Pendu</title>
-</head>
-<body>
-    <h1>Jeu du Pendu</h1>
-    <p>Mot à deviner : {{.WordToGuess}}</p>
-    <p>Essais restants : {{.GuessesRemaining}}</p>
-    <p>Lettrés incorrectes : {{.IncorrectGuesses}}</p>
-    <!-- Ajoutez ici l'affichage du pendu et un formulaire pour les propositions de lettres -->
-</body>
-</html>
-`
-
 	// Analysez le modèle HTML.
 	t, err := template.New("hangman").Parse(tmpl)
 	if err != nil {
