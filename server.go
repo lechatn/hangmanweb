0de3bb3aeeb02d7dffc247e73bdd4d7bd854a73e
lnoe <noe.lechat@ynov.com>
2023-11-06 22:00:16 +0100

    Ajout de la recuperation de la lettre envoyee

--- server.go
+++ server.go
@@ -3,12 +3,14 @@ package main
 import (
     "net/http"
 	"text/template"
+	"fmt"
 )
 
 func main() {
 
     http.HandleFunc("/", index)
 	http.HandleFunc("/game", game)
+	http.HandleFunc("/letter", handleLetter) // Définissez une route distincte pour la gestion de la lettre
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
     http.ListenAndServe(":8080", nil)
 }
@@ -27,4 +29,16 @@ func game(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 	tGame.Execute(w, nil)
+	
+}
+
+func handleLetter(w http.ResponseWriter, r *http.Request) {
+    if r.Method == http.MethodPost {
+        r.ParseForm()
+        lettre := r.PostFormValue("letterInput") // Utilisez "letterInput" au lieu de "test"
+        // Insérez ici le code de traitement de la lettre.
+        fmt.Fprintf(w, "La lettre saisie est : %s", lettre)
+    } else {
+        // Générez le formulaire ou redirigez si nécessaire.
+    }
 }
