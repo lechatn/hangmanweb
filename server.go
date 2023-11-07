6d8867b02340cb5f721c01b8201c2aa562e960bc
lnoe <noe.lechat@ynov.com>
2023-11-07 09:35:15 +0100

    Ajout du debut du jeu dans la balise div

--- server.go
+++ server.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-    "net/http"
-	"text/template"
 	"fmt"
 	"github.com/GuillaumeAntier/hangman"
+	"net/http"
+	"text/template"
 )
 
 func main() {
 
-    http.HandleFunc("/", index)
+	http.HandleFunc("/", index)
 	http.HandleFunc("/game", game)
 	http.HandleFunc("/letter", handleLetter) // Définissez une route distincte pour la gestion de la lettre
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-    http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
@@ -29,30 +29,23 @@ func game(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err)
 	}
-	 // Créez une variable dynamique en Go
-	 Dyna1:= "Trouvez le mot suivant : "
-	 Dyna2 := hangman.DisplayWord(hangman.RandomWord(hangman.LoadWords("base_de_donnée/words.txt")))
+	// Créez une variable dynamique en Go
+	Dyna2 := hangman.DisplayWord(hangman.RandomWord(hangman.LoadWords("base_de_donnée/words.txt")))
+	// Générez le contenu HTML avec la variable dynamique
+	htmlContent := fmt.Sprintf("%s", Dyna2)
+	data := struct{ Res string }{htmlContent}
+	// Écrivez la réponse HTML dans la sortie HTTP
+	tGame.Execute(w, data)
 
-	 // Générez le contenu HTML avec la variable dynamique
-	 htmlContent := fmt.Sprintf("<html><body><div id=\"game\"><p>%s</p></div></body></html>", Dyna1)
-	 htmlContent2 := fmt.Sprintf("<html><body><div id=\"game\"><p>%s</p></div></body></html>", Dyna2)
- 
-	 // Écrivez la réponse HTML dans la sortie HTTP
-	 w.Header().Set("Content-Type", "text/html")
-	 w.WriteHeader(http.StatusOK)
-	 w.Write([]byte(htmlContent))
-	 w.Write([]byte(htmlContent2))
-	tGame.Execute(w, nil)
-	
 }
 
 func handleLetter(w http.ResponseWriter, r *http.Request) {
-    if r.Method == http.MethodPost {
-        r.ParseForm()
-        lettre := r.PostFormValue("letterInput") // Utilisez "letterInput" au lieu de "test"
-        // Insérez ici le code de traitement de la lettre.
-        fmt.Fprintf(w, "La lettre saisie est : %s", lettre)
-    } else {
-        // Générez le formulaire ou redirigez si nécessaire.
-    }
+	if r.Method == http.MethodPost {
+		r.ParseForm()
+		lettre := r.PostFormValue("letterInput") // Utilisez "letterInput" au lieu de "test"
+		// Insérez ici le code de traitement de la lettre.
+		fmt.Fprintf(w, "La lettre saisie est : %s", lettre)
+	} else {
+		// Générez le formulaire ou redirigez si nécessaire.
+	}
 }
