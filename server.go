b475ccfcadb96da6024ee16d08442cf908e9fb1e
theo.delporte@ynov.com <theo.delporte@ynov.com>
2023-11-07 10:42:58 +0100

    mise en commun

--- server.go
+++ server.go
@@ -1,16 +1,24 @@
 package main
 
 import (
-    "net/http"
+	"fmt"
+	"github.com/GuillaumeAntier/hangman"
+	"net/http"
 	"text/template"
 )
 
 func main() {
+	word := hangman.DisplayWord(hangman.RandomWord(hangman.LoadWords("base_de_donnée/words.txt")))
 
-    http.HandleFunc("/", index)
-	http.HandleFunc("/game", game)
+	http.HandleFunc("/", index)
+	http.HandleFunc("/game", func(w http.ResponseWriter, r *http.Request) {
+		game(w, r, word)
+	})
+	http.HandleFunc("/letter", func(w http.ResponseWriter, r *http.Request) {
+		letter(w, r, word)
+	})
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-    http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
@@ -21,10 +29,50 @@ func index(w http.ResponseWriter, r *http.Request) {
 	tIndex.Execute(w, nil)
 }
 
-func game(w http.ResponseWriter, r *http.Request) {
+func game(w http.ResponseWriter, r *http.Request, word string) {
 	tGame, err := template.ParseFiles("game.html")
 	if err != nil {
 		panic(err)
 	}
-	tGame.Execute(w, nil)
+	// Créez une variable dynamique en Go
+	Dyna2 := word
+	// Générez le contenu HTML avec la variable dynamique
+	htmlContent := fmt.Sprintf("%s", Dyna2)
+	life := 10
+	htmlContent2 :=fmt.Sprintf("%d", life)
+	data := struct{
+		Res  string
+		Life string
+	}{
+		Res:  htmlContent,
+		Life: htmlContent2,
+	}
+	// Écrivez la réponse HTML dans la sortie HTTP
+	tGame.Execute(w, data)
+
+}
+
+func letter(w http.ResponseWriter, r *http.Request, word string) {
+	tletter, err := template.ParseFiles("game.html")
+	if err != nil {
+		panic(err)
+	}
+	// Créez une variable dynamique en Go
+	Dyna2 := word
+	// Générez le contenu HTML avec la variable dynamique
+	htmlContent := fmt.Sprintf("%s", Dyna2)
+	life := 10
+	life --
+	htmlContent2 :=fmt.Sprintf("%d", life)
+	data := struct{
+		Res  string
+		Life string
+	}{
+		Res:  htmlContent,
+		Life: htmlContent2,
+	}
+	tletter.Execute(w, data)
+	// Écrivez la réponse HTML dans la sortie HTTP
+    //lettre := r.PostFormValue("letterInput")
+
 }
