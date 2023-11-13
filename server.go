0dfcb380431e88b112422a3d383536c8b322af2c
lnoe <noe.lechat@ynov.com>
2023-11-13 15:00:57 +0100

    Ajout des nouveaux fichier png des differents niveau du pendu, ajout de lose.html lorsuq'on a perdu, ajout du bouton rejouer pour recommencer la partie et différentes modifs sur le style de la page

--- server.go
+++ server.go
@@ -3,25 +3,27 @@ package main
 import (
 	"fmt"
 	"github.com/lechatn/hangman"
-	"io/ioutil"
 	"net/http"
 	"strings"
 	"text/template"
+	"strconv"
 )
 
 func main() {
 	word := hangman.RandomWord(hangman.LoadWords("base_de_donnée/words.txt"))
 	print(word, "\n")
 	Display := hangman.DisplayWord(word)
-	IndexHangman := 0
 	Failed_letter := ""
 	life := 10
 	http.HandleFunc("/", index)
 	http.HandleFunc("/game", func(w http.ResponseWriter, r *http.Request) {
 		game(w, r, Display, life)
 	})
+	http.HandleFunc("/restart", func(w http.ResponseWriter, r *http.Request) {
+		Display, life, Failed_letter,word = restart(w, r, word, life, Display, Failed_letter)
+	})
 	http.HandleFunc("/letter", func(w http.ResponseWriter, r *http.Request) {
-		Display, life, IndexHangman, Failed_letter = letter(w, r, word, life, Display, IndexHangman, Failed_letter)
+		Display, life, Failed_letter = letter(w, r, word, life, Display, Failed_letter)
 	})
 	http.HandleFunc("/regle", func(w http.ResponseWriter, r *http.Request) {
 		regle(w, r)
@@ -60,8 +62,9 @@ func game(w http.ResponseWriter, r *http.Request, Display string, life int) {
 
 }
 
-func letter(w http.ResponseWriter, r *http.Request, word string, life int, Display string, IndexHangman int, Failed_letter string) (string, int, int, string) {
+func letter(w http.ResponseWriter, r *http.Request, word string, life int, Display string, Failed_letter string) (string, int, string) {
 	tletter, err := template.ParseFiles("letter.html")
+	IndexHangman := 0
 	if err != nil {
 		panic(err)
 	}
@@ -70,26 +73,21 @@ func letter(w http.ResponseWriter, r *http.Request, word string, life int, Displ
 
 	// Écrivez la réponse HTML dans la sortie HTTP
 	letter := r.PostFormValue("letterInput")
-	file, err := ioutil.ReadFile("affichage/hangman.txt")
-	Display, life, IndexHangman, Failed_letter = hangman.IsPresent(strings.ToUpper(letter), word, Display, life, IndexHangman, Failed_letter)
+	Display, life,IndexHangman, Failed_letter = hangman.IsPresent(strings.ToUpper(letter), word, Display, life, IndexHangman, Failed_letter)
 	htmlContent := fmt.Sprintf("%s", Display)
 	htmlContent2 := fmt.Sprintf("%d", life)
 	htmlContent3 := fmt.Sprintf("%s", word)
 	htmlContent4 := ""
 	htmlContent5 := fmt.Sprintf("%s", Failed_letter)
-	lines := strings.Split(string(file), "\n") // We cut the file hangman.txt line by line
-	if life == 10 {
-		for i := 0; i < 7; i++ {
-			htmlContent4 = htmlContent4 + lines[i] + "<br>"
-		}
-	} else {
-		for i := IndexHangman; i < IndexHangman+7; i++ {
-			htmlContent4 = htmlContent4 + lines[i] + "<br>"
-		}
-	}
+	htmlContent6 := "static/images/hangman-"+strconv.Itoa(9-life)+".png"
+	print(htmlContent6)
 	if word == Display {
 		win(w, r)
-		return Display, life, IndexHangman, Failed_letter
+		return Display, life, Failed_letter
+	}
+	if life == 0 {	
+		loose(w, r, word)
+		return Display, life, Failed_letter
 	}
 	data := struct {
 		Display       string
@@ -97,16 +95,18 @@ func letter(w http.ResponseWriter, r *http.Request, word string, life int, Displ
 		Word          string
 		IndexHangman  string
 		Failed_letter string
+		ImageName     string
 	}{
 		Display:       htmlContent,
 		Life:          htmlContent2,
 		Word:          htmlContent3,
 		IndexHangman:  htmlContent4,
 		Failed_letter: htmlContent5,
+		ImageName:     htmlContent6,
 	}
 	tletter.Execute(w, data)
 
-	return Display, life, IndexHangman, Failed_letter
+	return Display, life, Failed_letter
 }
 
 func win(w http.ResponseWriter, r *http.Request) {
@@ -123,4 +123,36 @@ func regle(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 	tRegles.Execute(w, nil)
+}
+
+func restart(w http.ResponseWriter, r *http.Request, word string, life int, Display string, Failed_letter string) (string, int, string, string){
+	tRestart, err := template.ParseFiles("game.html")
+	if err != nil {
+		panic(err)
+	}
+	word = hangman.RandomWord(hangman.LoadWords("base_de_donnée/words.txt"))
+	Display = hangman.DisplayWord(word)
+	Failed_letter = ""
+	life = 10
+	htmlContent := fmt.Sprintf("%s", Display)
+	htmlContent2 := fmt.Sprintf("%d", life)
+	data := struct {	
+		Display string
+		Life    string
+	}{
+		Display: htmlContent,
+		Life:    htmlContent2,
+	
+	}
+	tRestart.Execute(w, data)
+
+	return Display,life, Failed_letter,word
+}
+
+func loose(w http.ResponseWriter, r *http.Request, word string) {
+	tWin, err := template.ParseFiles("loose.html")
+	if err != nil {
+		panic(err)
+	}
+	tWin.Execute(w, nil)
 }
