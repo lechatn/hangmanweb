240b9329dc064387b05b7e2fe507044f4279a5e4
anajmeld <najmeldeen.adamabkar@ynov.com>
2023-11-14 07:46:57 +0100

    tmple

--- server.go
+++ server.go
@@ -1,56 +1,14 @@
-/*
 package main
 
 import (
-
-	"net/http"
-	"text/template"
-
-)
-
-func main() {
-
-		http.HandleFunc("/", index)
-		http.HandleFunc("/game", game)
-		http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-		http.ListenAndServe(":8080", nil)
-	}
-
-	func index(w http.ResponseWriter, r *http.Request) {
-		tIndex, err := template.ParseFiles("index.html")
-		if err != nil {
-			panic(err)
-		}
-		tIndex.Execute(w, nil)
-	}
-
-	func game(w http.ResponseWriter, r *http.Request) {
-		tGame, err := template.ParseFiles("game.html")
-		if err != nil {
-			panic(err)
-		}
-		tGame.Execute(w, nil)
-	}
-*/
-package main
-
-import (
-	"fmt"
-	"github.com/GuillaumeAntier/hangman"
 	"net/http"
 	"text/template"
 )
 
 func main() {
-	word := hangman.DisplayWord(hangman.RandomWord(hangman.LoadWords("base_de_donnée/words.txt")))
 
 	http.HandleFunc("/", index)
-	http.HandleFunc("/game", func(w http.ResponseWriter, r *http.Request) {
-		game(w, r, word)
-	})
-	http.HandleFunc("/letter", func(w http.ResponseWriter, r *http.Request) {
-		letter(w, r, word)
-	})
+	http.HandleFunc("/game", game)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.ListenAndServe(":8080", nil)
 }
@@ -63,50 +21,10 @@ func index(w http.ResponseWriter, r *http.Request) {
 	tIndex.Execute(w, nil)
 }
 
-func game(w http.ResponseWriter, r *http.Request, word string) {
+func game(w http.ResponseWriter, r *http.Request) {
 	tGame, err := template.ParseFiles("game.html")
 	if err != nil {
 		panic(err)
 	}
-	// Créez une variable dynamique en Go
-	Dyna2 := word
-	// Générez le contenu HTML avec la variable dynamique
-	htmlContent := fmt.Sprintf("%s", Dyna2)
-	life := 10
-	htmlContent2 := fmt.Sprintf("%d", life)
-	data := struct {
-		Res  string
-		Life string
-	}{
-		Res:  htmlContent,
-		Life: htmlContent2,
-	}
-	// Écrivez la réponse HTML dans la sortie HTTP
-	tGame.Execute(w, data)
-
-}
-
-func letter(w http.ResponseWriter, r *http.Request, word string) {
-	tletter, err := template.ParseFiles("game.html")
-	if err != nil {
-		panic(err)
-	}
-	// Créez une variable dynamique en Go
-	Dyna2 := word
-	// Générez le contenu HTML avec la variable dynamique
-	htmlContent := fmt.Sprintf("%s", Dyna2)
-	life := 10
-	life--
-	htmlContent2 := fmt.Sprintf("%d", life)
-	data := struct {
-		Res  string
-		Life string
-	}{
-		Res:  htmlContent,
-		Life: htmlContent2,
-	}
-	tletter.Execute(w, data)
-	// Écrivez la réponse HTML dans la sortie HTTP
-	//lettre := r.PostFormValue("letterInput")
-
+	tGame.Execute(w, nil)
 }
